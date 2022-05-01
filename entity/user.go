827fadea06fc3b4d9763abbd5abb47d1fcc5141b
o-t-k-t <wetcondition@gmail.com>
2022-05-01 18:37:56 +0900

    import into domain with defined type

--- entity/user.go
+++ entity/user.go
@@ -2,10 +2,8 @@ package entity
 
 import "github.com/o-t-k-t/sql_boiler_exercise/models"
 
-type User struct {
-	User *models.User
-}
+type User models.User
 
 func (u User) LocalName() string {
-	return u.User.Name + "さん"
+	return u.Name + "さん"
 }
